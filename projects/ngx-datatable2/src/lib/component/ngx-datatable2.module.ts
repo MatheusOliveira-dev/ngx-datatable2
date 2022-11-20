ca6c069e6aaed6064941d02ad680b0d20a9381bd
Matheus Oliveira <matheus.oliveira22002@gmail.com>
2022-11-20 03:37:59 -0300

    Initial Commit

---
+++ projects/ngx-datatable2/src/lib/component/ngx-datatable2.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {CommonModule} from "@angular/common";
+import { NgxPaginationModule } from 'ngx-pagination';
+
+import { NgxDatatableComponent } from './ngx-datatable2.component';
+import { CaptionComponent } from './caption/caption.component';
+
+
+
+import { SearchPipe } from '../pipe/search.pipe';
+import { SortPipe } from '../pipe/sort.pipe';
+
+import { DataShowingService } from '../services/data-showing.service';
+
+@NgModule({
+  declarations: [NgxDatatableComponent, SearchPipe, SortPipe, CaptionComponent],
+  imports: [
+        FormsModule,
+        CommonModule,
+        NgxPaginationModule
+    ],
+    providers: [],
+    exports: [NgxDatatableComponent, CaptionComponent]
+})
+export class NgxDatatableModule { }
---
+++ projects/ngx-datatable2/src/public-api.ts
@@ -0,0 +1,12 @@
+/*
+ * Public API Surface of ngx-datatable2
+ */
+
+
+export * from './lib/component/ngx-datatable2.component';
+export * from './lib/component/ngx-datatable2.module';
+
+// export * from './lib/services/data-showing.service';
+
+// export * from './lib/pipe/search.pipe';
+// export * from './lib/pipe/sort.pipe';
